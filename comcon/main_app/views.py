88377e93d44eb0220cd5608e74fc69a796364082
Albert Chung <chung972@gmail.com>
2019-06-15 16:54:33 -0500

    adds signup view function; adds TODO comments

--- comcon/main_app/views.py
+++ comcon/main_app/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+# TODO:
+#  - we still need to implement login_required and LoginRequiredMixin
+#  - also look at steps 3, 8, and 9(10) in the lab
+#  - keep in mind WHERE we want to send the user after a successful login/out (change in settings)
+#  for specifics, just refer to the lab:
+#  https://git.generalassemb.ly/SEI-CC/SEI-CC-2/blob/master/work/w08/d4/02-03-django-authentication/django-authentication.md
 
 
 def home(request):
@@ -7,3 +18,23 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        # This is how to create a 'user' form object
+        # that includes the data from the browser
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # This will add the user to the database
+            user = form.save()
+            # This is how we log a user in via code
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    # A bad POST or a GET request, so render signup.html with an empty form
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
