5015082a6b37533ac781b7a2b397a9344d96b1ca
Albert Chung <chung972@gmail.com>
2019-06-15 15:09:15 -0500

    wraps database name in single quotation marks

--- comcon/comcon/settings.py
+++ comcon/comcon/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'comcon.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': comcon,
+        'NAME': 'comcon',
     }
 }
 
