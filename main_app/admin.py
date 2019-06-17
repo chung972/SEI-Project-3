f4869285f79c6e19aeb1cae0102ef13da9849654
Albert Chung <chung972@gmail.com>
2019-06-17 14:08:27 -0500

    registers Profile model to admin portal

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Event
+from .models import Profile, Event
 
 # Register your models here.
+admin.site.register(Profile)
 admin.site.register(Event)
