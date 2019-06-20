c6469103074c3738ec3c4be7304ab6942885b100
Albert Chung <chung972@gmail.com>
2019-06-20 17:42:05 -0500

    installs BigIntegerField;changes phone_number field to be BigInteger; adds user attribute to Photo model

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.db.models.fields import BigIntegerField
 
 # Create your models here.
 
@@ -9,7 +10,7 @@ from django.db.models.signals import post_save
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     affiliations = models.CharField(max_length=100)
-    phone_number = models.IntegerField()
+    phone_number = models.BigIntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.user.username
@@ -52,6 +53,7 @@ class Event(models.Model):
 class Photo(models.Model):
     url = models.CharField(max_length=200)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Photo for event_id: {self.event_id} @{self.url}"
